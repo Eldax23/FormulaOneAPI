acaa40598259a70e6a4e62ec5aa8f8341c833333
eldax <mohamedabu.el3ela@gmail.com>
2025-10-01 23:40:00 +0300

    Adding Entites

---
+++ Entites/Driver.cs
@@ -0,0 +1,13 @@
+﻿namespace Entites;
+
+public class Driver : BaseEntity
+{
+
+
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } =  string.Empty;
+    public int DriverNumber { get; set; }
+    public DateTime DateOfBirth { get; set; }
+
+    public ICollection<Achievement> Achievements { get; set; } = new HashSet<Achievement>();
+}
